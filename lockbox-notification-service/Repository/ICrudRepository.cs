4e46d111eda44bdcfb269b0a57b5654143cf5a90
Rik de Goede <rik.dgd@gmail.com>
2025-05-25 00:37:34 +0200

    Remove repository structure, too much overhead.

--- lockbox-notification-service/Repository/ICrudRepository.cs
+++
@@ -1,9 +0,0 @@
-namespace lockbox_notification_service.Repository;
-
-public interface ICrudRepository<TModel>
-{
-    void Create(TModel model);
-    TModel? Read(string id);
-    void Update(TModel model);
-    void Delete(string id);
-}
